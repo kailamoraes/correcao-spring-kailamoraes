4e2b9a065f50a804b92e13f1669a3ba024cfa944
kailamoraes <kailaleontinademoraes@gmail.com>
2022-10-03 16:50:52 +0000

    correcao

---
+++ app/src/main/java/kailamoraes/repositories/AlunosRepository.java
@@ -0,0 +1,6 @@
+package kailamoraes.repositories; 
+import org.springframework.data.repository.CrudRepository; 
+import kailamoraes.models.Aluno; 
+public interface AlunosRepository extends CrudRepository<Aluno, Integer> { 
+ 
+} 
