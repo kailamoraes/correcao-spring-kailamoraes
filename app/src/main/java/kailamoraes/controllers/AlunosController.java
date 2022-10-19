3bad04d28015ebfdd1f5e06c4eae0e238f704cda
kailamoraes <kailaleontinademoraes@gmail.com>
2022-10-19 17:50:52 +0000

    19-10

--- app/src/main/java/kailamoraes/controllers/AlunosController.java
+++ app/src/main/java/kailamoraes/controllers/AlunosController.java
@@ -1,56 +1,72 @@
-package kailamoraes.controllers; 
+package kailamoraes.controllers;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired; 
-import org.springframework.stereotype.Controller; 
-import org.springframework.ui.Model; 
-import org.springframework.web.bind.annotation.RequestMapping; 
-import org.springframework.web.bind.annotation.RequestMethod; 
-import org.springframework.web.bind.annotation.RequestParam; 
-import kailamoraes.models.Aluno; 
-import kailamoraes.repositories.AlunosRepository; 
-import org.springframework.web.bin.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import kailamoraes.models.Aluno;
+import kailamoraes.repositories.AlunosRepository;
 
 @Controller
-@RequestMapping("/alunos") 
-public class AlunosController { 
-     @Autowired
-     private AlunosRepository alunosRepo; 
+@RequestMapping("/alunos")
+public class AlunosController {
+    @Autowired
+    private AlunosRepository alunosRepo;
 
-    @RequestMapping("list") 
-    public String list(Model model) { 
-    model.addAttribute("alunos", this.alunosRepo.findAll()); 
-    return "list"; 
- } 
+    @RequestMapping("list")
+    public String list(Model model) {
+        model.addAttribute("alunos", this.alunosRepo.findAll());
+        return "list";
+    }
 
-    @RequestMapping("insert") 
-    public String insert() { 
-        return "insert"; 
- } 
- 
-    @RequestMapping(value = "insert", method = RequestMethod.POST) 
-    public String insert(@RequestParam("nome") String nome, @RequestParam("idade") int idade) { 
-    Aluno aluno = new Aluno(); 
-    aluno.setNome(nome); 
-    aluno.setIdade(idade); 
-    alunosRepo.save(aluno); 
-    return "redirect:/alunos/list"; 
-    } 
+    @RequestMapping("insert")
+    public String insert() {
+        return "insert";
+    }
+
+    @RequestMapping(value = "insert", method = RequestMethod.POST)
+    public String insert(@RequestParam("nome") String nome, @RequestParam("idade") int idade) {
+        Aluno aluno = new Aluno();
+        aluno.setNome(nome);
+        aluno.setIdade(idade);
+        alunosRepo.save(aluno);
+        return "redirect:/alunos/list";
+    }
 
     @RequestMapping("update/{id}")
-    public String update(Model model @PathVariable int id) {
+    public String update(Model model, @PathVariable int id) {
         Optional<Aluno> aluno = alunosRepo.findById(id);
         model.addAttribute("aluno", aluno.get());
         return "/alunos/update";
     }
 
-    @RequestMapping(value = "update", method = RequetMethod.POST)
-    public String saveUpdate(@RequestParam("nome") String nome,@RequestParam("idade") int idade,@RequestParam("id") int id) {
-        Optional<aluno> aluno = alunosRepo.findById(id);
-        aluno.get().setNome(nome);
-        aluno.get().setIdade(idade);
-        alunosRepo.save(aluno.get());
+    @RequestMapping(value = "update", method = RequestMethod.POST)
+    public String saveUpdate(
+        @RequestParam("nome") String nome,
+        @RequestParam("idade") int idade,
+        @RequestParam("id") int id) {
+            Optional<Aluno> aluno = alunosRepo.findById(id);
+            aluno.get().setNome(nome);
+            aluno.get().setIdade(idade);
+            alunosRepo.save(aluno.get());
+            return "redirect:/alunos/list";
+    }
+    
+    @RequestMapping("delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        Optional<Aluno> aluno = alunosRepo.findById(id);
+        model.addAttribute("aluno", aluno.get());
+        return "/alunos/delete";
+    }
+
+    @RequestMapping(value = "delete", method = RequestMethod.POST)
+    public String saveDelete(@RequestParam("id") int id) {
+        alunosRepo.deleteById(id);
         return "redirect:/alunos/list";
     }
-} 
+}
--- app/src/main/java/kailamoraes/repositories/AlunosRepository.java
+++ app/src/main/java/kailamoraes/repositories/AlunosRepository.java
@@ -1,6 +1,7 @@
-package kailamoraes.repositories; 
-import org.springframework.data.repository.CrudRepository; 
-import kailamoraes.models.Aluno; 
-public interface AlunosRepository extends CrudRepository<Aluno, Integer> { 
- 
-} 
+package kailamoraes.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import kailamoraes.models.Aluno;
+
+public interface AlunosRepository extends CrudRepository<Aluno, Integer> {
+}
